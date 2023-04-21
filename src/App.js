7f2a888bd8545c5f984a53f4c5904ae9ea85411a
Abel De Leon <abelefai@gmail.com>
2023-04-21 16:01:11 -0700

    routing

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/smallbusiness' element={<HomePage />} />
-        <Route path='/smallbusiness/About' element={<AboutPage />} />
+        <Route path='/smallbusiness/about' element={<AboutPage />} />
       </Routes>
       <AppFooter />
     </div>
